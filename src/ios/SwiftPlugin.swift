642672e968c70759e023871e6dfa7265b9eda8cb
roeyHonig <roeyhonig94@gmail.com>
2018-10-20 18:50:16 +0300

    add _ to the function parmeter name, it is needed in order to the function to be recognized

--- src/ios/SwiftPlugin.swift
+++ src/ios/SwiftPlugin.swift
@@ -1,13 +1,16 @@
 @objc(ObjcPlugin) class ObjcPlugin : CDVPlugin {
   
-  func coolMethod(command: CDVInvokedUrlCommand) {
+   func coolMethod(_ command: CDVInvokedUrlCommand) {
       print("hello, this time from native Swift")
 
     var pluginResult = CDVPluginResult(
       status: CDVCommandStatus_ERROR
     )
-
-    let msg = command.arguments[0] as? String ?? ""
+    
+    
+    print("did something came?: \((command.arguments[0] as! [Any])[0])")
+    
+    let msg = (command.arguments[0] as! [Any])[0] as? String ?? ""
     print("here is the argument that was recived:  \(msg)")
 
     if msg.characters.count > 0 {

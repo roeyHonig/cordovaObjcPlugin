d76805c5dfae19d96e35300dfcd0a1e1ca0e1fdf
roeyHonig <37895849+roeyHonig@users.noreply.github.com>
2018-10-21 11:44:28 +0300

    added to the iOS platforme

---
+++ src/ios/PlaceFramework.framework/Headers/PlaceFramework.h
@@ -0,0 +1,19 @@
+//
+//  PlaceFramework.h
+//  PlaceFramework
+//
+//  Created by MSApps on 17/10/2018.
+//  Copyright © 2018 MSApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PlaceFramework.
+FOUNDATION_EXPORT double PlaceFrameworkVersionNumber;
+
+//! Project version string for PlaceFramework.
+FOUNDATION_EXPORT const unsigned char PlaceFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PlaceFramework/PublicHeader.h>
+
+
